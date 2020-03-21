9f37dedad7798fc3e87c8813f0baa5e4a03a1b46
Norman Bartel <dev@normanbartel.de>
2020-03-21 21:56:07 +0100

    Added draft plus first content.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  filters: string[] = [];
+  selectedFilters: string[] = [];
+
+  articles: string[] = ['1', '2', '3'];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.filters.push('COVID-19');
+    this.filters.push('SCHULAUSFALL');
+    this.filters.push('AUSGANGSSPERRE');
+    this.filters.push('THEMA3');
+    this.filters.push('THEMA4');
+    this.filters.push('THEMA5');
+    this.filters = this.filters.filter((item, i, ar) => ar.indexOf(item) === i); //make it unique
+    this.filters.sort(); // make it sorted
   }
+
+  toggleFilter(filter: string): void {
+    if(this.selectedFilters.includes(filter)) {
+        this.selectedFilters = this.selectedFilters.filter(item => item !== filter)
+        this.filters.push(filter);
+    } else {
+        this.filters = this.filters.filter(item => item !== filter)
+        this.selectedFilters.push(filter);
+    }
+    this.filters = this.filters.filter((item, i, ar) => ar.indexOf(item) === i); //make it unique
+    this.filters.sort(); // make it sorted
+
+    this.selectedFilters = this.selectedFilters.filter((item, i, ar) => ar.indexOf(item) === i); //make it unique
+    this.selectedFilters.sort(); // make it sorted
+  }
+
 
 }
